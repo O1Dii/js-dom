3da21e027d816e6a62e0ada353eeda7a3b9a053d
O1Dii <oldi730051@mail.ru>
2019-07-15 15:05:21 +0300

    append, remove done

--- custom-jQuery.js
+++ custom-jQuery.js
@@ -1,84 +1,106 @@
-const addClass = function (classes) {
-    if (typeof classes === 'string' || typeof classes === 'function') {
-        for (const each in this) {
-            if (this.hasOwnProperty(each)) {
-                let addition;
-                if (typeof classes === 'function') {
-                    addition = classes(each, this[each].className);
-                }
-                else {
-                    addition = classes;
-                }
-                if (typeof addition === 'string') {
-                    if (this[each].className) {
-                        for (const each_class of addition.split(' ')) {
-                            if (this[each].className.indexOf(each_class) === -1) {
-                                this[each].className += ' ' + each_class;
-                            }
-                        }
+class JQuery {
+    constructor(selector) {
+        this.selector = selector;
+        this.elements = document.querySelectorAll(selector);
+    }
+
+    addClass(classes) {
+        if (typeof classes === 'string' || typeof classes === 'function') {
+            for (const each in this.elements) {
+                if (this.elements.hasOwnProperty(each)) {
+                    let addition;
+                    if (typeof classes === 'function') {
+                        addition = classes(each, this.elements[each].className);
                     }
                     else {
-                        this[each].className = addition;
+                        addition = classes;
                     }
-                }
-            }
-        }
-    }
-    return this;
-};
-
-const removeClass = function (classes) {
-    if (typeof classes === 'string' || typeof classes === 'function') {
-        for (const each in this) {
-            if (this.hasOwnProperty(each)) {
-                let removing;
-                if (typeof classes === 'function') {
-                    removing = classes(each, this[each].className);
-                }
-                else {
-                    removing = classes;
-                }
-                if (typeof removing === 'string') {
-                    if (this[each].className) {
-                        this[each].className += ' '
-                        for (const each_class of removing.split(' ')) {
-                            if (this[each].className.indexOf(each_class + ' ') !== -1) {
-                                this[each].className = this[each].className.replace(each_class + ' ', '');
+                    if (typeof addition === 'string') {
+                        if (this.elements[each].className) {
+                            for (const each_class of addition.split(' ')) {
+                                if (this.elements[each].className.indexOf(each_class) === -1) {
+                                    this.elements[each].className += ' ' + each_class;
+                                }
                             }
+                        }
+                        else {
+                            this.elements[each].className = addition;
                         }
                     }
                 }
-                this[each].className = this[each].className.trim();
             }
         }
+        return this;
     }
-    return this;
-};
 
-const append = function (...elements) {
-    while (elements.some((value) => typeof value[Symbol.iterator] === 'function' && typeof value !== 'string')) {
-        elements = elements.flat();
+    removeClass(classes) {
+        if (typeof classes === 'string' || typeof classes === 'function') {
+            for (const each in this.elements) {
+                if (this.elements.hasOwnProperty(each)) {
+                    let removing;
+                    if (typeof classes === 'function') {
+                        removing = classes(each, this.elements[each].className);
+                    }
+                    else {
+                        removing = classes;
+                    }
+                    if (typeof removing === 'string') {
+                        if (this.elements[each].className) {
+                            this.elements[each].className += ' '
+                            for (const each_class of removing.split(' ')) {
+                                if (this.elements[each].className.indexOf(each_class + ' ') !== -1) {
+                                    this.elements[each].className = this.elements[each].className.replace(each_class + ' ', '');
+                                }
+                            }
+                        }
+                    }
+                    this.elements[each].className = this.elements[each].className.trim();
+                }
+            }
+        }
+        return this;
     }
-    elements = elements.flat();
-    for (const each of this) {
-        for (const each_elem of elements) {
-            const elem = document.createElement(each_elem.substring(1, each_elem.indexOf('>')));
-            elem.innerHTML = each_elem.substring(each_elem.indexOf('>') + 1, each_elem.lastIndexOf('<'));
-            console.log(elem);
-            each.appendChild(elem);
+
+    append(...elements) {
+        if (elements.length === 1 && typeof elements[0] === 'function') {
+            for (const each in this.elements) {
+                if (this.elements.hasOwnProperty(each)) {
+                    elements[0](each, this.elements[each].innerHTML);
+                }
+            }
+        }
+        elements = elements.flat();
+        for (const each of this.elements) {
+            console.log(each);
+            for (const each_elem of elements) {
+                if (typeof each_elem === 'Element') {
+                    each.appendChild(each_elem);
+                }
+                else if (each_elem instanceof JQuery) {
+                    for (let elem of each_elem.elements) {
+                        each.appendChild(elem);
+                    }
+                }
+                else {
+                    each.innerHTML += each_elem;
+                }
+            }
+        }
+        return this;
+    }
+
+    remove(selector = '') {
+        selector = this.selector + selector;
+        const elements = document.querySelectorAll(selector);
+        for (const element of elements) {
+            element.parentNode.removeChild(element);
         }
     }
-    return this;
 }
 
 function $(selector) {
     if (typeof selector === 'string') {
-        let res = document.querySelectorAll(selector);
-        let prot = Object.getPrototypeOf(res);
-        prot.addClass = addClass.bind(res);
-        prot.removeClass = removeClass.bind(res);
-        prot.append = append.bind(res);
-        return res;
+        return new JQuery(selector);
     }
     return null;
 }
